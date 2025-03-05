b103ec2dd1c745a99a8668971b86b737d5f46402
JackBreeton <jackbreeton@gmail.com>
2025-03-05 14:08:15 +0000

    Initial commit

---
+++ wp-content/mu-plugins/aios-firewall-loader.php
@@ -0,0 +1,6 @@
+<?php
+// Begin AIOWPSEC Firewall
+if (file_exists('/opt/bitnami/wordpress/aios-bootstrap.php')) {
+	include_once('/opt/bitnami/wordpress/aios-bootstrap.php');
+}
+// End AIOWPSEC Firewall
